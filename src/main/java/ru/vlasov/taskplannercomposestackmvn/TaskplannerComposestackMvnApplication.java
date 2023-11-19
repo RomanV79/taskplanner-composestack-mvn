48217396ba6d366c0a87895a54b215e22f13cd37
RomanV79 <121058298+RomanV79@users.noreply.github.com>
2023-11-19 16:21:25 +0300

    Initial commit

---
+++ src/main/java/ru/vlasov/taskplannercomposestackmvn/TaskplannerComposestackMvnApplication.java
@@ -0,0 +1,13 @@
+package ru.vlasov.taskplannercomposestackmvn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskplannerComposestackMvnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskplannerComposestackMvnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/vlasov/taskplannercomposestackmvn/TaskplannerComposestackMvnApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vlasov.taskplannercomposestackmvn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskplannerComposestackMvnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
